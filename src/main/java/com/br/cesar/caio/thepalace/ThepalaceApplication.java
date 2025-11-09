006f799842d905c6f46b1d548966a243c31c015a
CaioLira18 <caiozao1212@gmail.com>
2025-11-08 21:35:46 -0300

    [fea] - update

---
+++ src/main/java/com/br/cesar/caio/thepalace/ThepalaceApplication.java
@@ -0,0 +1,13 @@
+package com.br.cesar.caio.thepalace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThepalaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThepalaceApplication.class, args);
+	}
+
+}
